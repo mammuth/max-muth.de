64aa84dda7410be4d6a6101b952198ef3f59656a
Max Muth <kontakt@maxi-muth.de>
2025-07-05 18:50:43 +0200

    fix: more harmonic auto darkmode and toggle

--- themes/archie/static/js/themetoggle.js
+++ themes/archie/static/js/themetoggle.js
@@ -1,22 +1,25 @@
 function setTheme(mode) {
-    localStorage.setItem("theme-storage", mode);
-    var e = document.querySelector("#dark-mode-toggle > .feather > use")
-    if (mode === "dark") {
-        document.getElementById("darkModeStyle").disabled=false;
-	e.href.baseVal = e.href.baseVal.replace(/#.*$/, "#sun")
-    } else if (mode === "light") {
-        document.getElementById("darkModeStyle").disabled=true;
-	e.href.baseVal = e.href.baseVal.replace(/#.*$/, "#moon")
-    }
+  localStorage.setItem("theme-storage", mode);
+  var e = document.querySelector("#dark-mode-toggle > .feather > use");
+  const styleLink = document.getElementById("darkModeStyle");
+  if (mode === "dark") {
+    styleLink.disabled = false;
+    styleLink.media = "all";
+    e.href.baseVal = e.href.baseVal.replace(/#.*$/, "#sun");
+  } else if (mode === "light") {
+    styleLink.disabled = true;
+    styleLink.media = "not all";
+    e.href.baseVal = e.href.baseVal.replace(/#.*$/, "#moon");
+  }
 }
 
 function toggleTheme() {
-    if (localStorage.getItem("theme-storage") === "light") {
-        setTheme("dark");
-    } else if (localStorage.getItem("theme-storage") === "dark") {
-        setTheme("light");
-    }
+  if (localStorage.getItem("theme-storage") === "light") {
+    setTheme("dark");
+  } else if (localStorage.getItem("theme-storage") === "dark") {
+    setTheme("light");
+  }
 }
 
-var savedTheme = localStorage.getItem("theme-storage") || "light";
+var savedTheme = localStorage.getItem("theme-storage") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 setTheme(savedTheme);
